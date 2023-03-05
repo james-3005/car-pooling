9d7ebe53f8059c32f29f6dd024d3c9563f911fe6
huytn <huytn@kaopiz.com>
2023-03-05 22:52:03 +0700

    demo frequent

--- src/views/Admin/Cluster.vue
+++ src/views/Admin/Cluster.vue
@@ -8,12 +8,12 @@
         @click="handleClick"
       >
         <RoutingMultipleMarkerWithColor2
-          v-for="(user, index) in listUser"
+          v-for="(time, index) in form.listTime"
           :key="index"
-          :color="user.color"
-          :waypoints="user.location"
+          :color="listUser[time].color"
+          :waypoints="listUser[time].locations"
           @updateLocation="(value) => updateLocation(value, index)"
-          v-if="user.location.length"
+          v-if="listUser[time].locations.length && tab === 0"
         />
         <gmap-marker
           v-if="mapCursor"
@@ -41,7 +41,7 @@
         <Instruction />
       </div>
     </div>
-    <div class="white elevation-5 pa-2 overflow-auto" style="width: 400px">
+    <div class="white elevation-5 pa-2 overflow-auto" style="width: 500px">
       <v-tabs
         v-model="tab"
         grow
@@ -66,35 +66,43 @@
           <h6 class="my-2">1.1 Thời điểm bắt đầu</h6>
           <v-row>
             <v-col cols="6">
-              <DatePicker hide-details label="Ngày" v-model="form.date" />
+              <DatePicker label="Ngày" v-model="form.date" hide-details />
             </v-col>
-            <v-col cols="4">
-              <TimePicker hide-details label="Giờ" v-model="form.time" />
+            <v-col cols="4" class="px-0">
+              <TimePicker label="Giờ" v-model="form.time" hide-details />
             </v-col>
-            <v-col cols="2" class="d-flex align-center"
-              >{{ form.interval }}p
+            <v-col cols="1" class="d-flex align-center"
+              >{{ form.interval }}min
+            </v-col>
+            <v-col cols="1" class="d-flex align-center">
+              <v-btn icon color="primary" @click="addTime">
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
             </v-col>
           </v-row>
           <div>
             <v-row
               class="rowCustom"
-              v-for="(user, index) in listUser"
+              v-for="(time, index) in form.listTime"
               :key="index"
             >
-              <v-col cols="5">
+              <v-col cols="6">
                 <v-text-field
                   class="pt-0"
                   label="Tên"
                   hide-details
-                  v-model="user.user_id"
+                  :value="time"
+                  readonly
+                  outlined
+                  dense
                 />
               </v-col>
-              <v-col cols="4">
+              <v-col cols="3">
                 <v-select
                   class="pt-0"
                   label="Màu"
                   hide-details
-                  v-model="user.color"
+                  v-model="listUser[time].color"
                   :items="COLOR_PALETTE"
                 >
                   <template v-slot:selection="{ item, index }">
@@ -111,71 +119,58 @@
               </v-col>
               <v-col cols="3">
                 <v-btn icon color="primary" small>
-                  <v-icon small @click="addCurrentLocation(index)"
+                  <v-icon small @click="addCurrentHistory(time)"
                     >mdi-crosshairs-gps
                   </v-icon>
                 </v-btn>
-                <v-btn icon color="error" small @click="clearLocation(index)">
+                <v-btn icon color="error" small @click="clearHistory(time)">
                   <v-icon small>mdi-restore</v-icon>
                 </v-btn>
                 <v-btn
                   icon
                   color="error"
                   small
-                  @click="() => deleteUser(index)"
+                  @click="() => deleteHistory(time)"
                 >
                   <v-icon small>mdi-delete</v-icon>
                 </v-btn>
               </v-col>
             </v-row>
-            <v-btn
-              block
-              color="primary"
-              small
-              class="mt-4"
-              @click="
-                () => {
-                  listUser = [...listUser, { location: [] }];
-                }
-              "
-            >
-              <v-icon small>mdi-plus</v-icon>
-              Thêm người
-            </v-btn>
+            <!--            <v-btn-->
+            <!--              block-->
+            <!--              color="primary"-->
+            <!--              small-->
+            <!--              class="mt-4"-->
+            <!--              @click="initSetLocation"-->
+            <!--              :disabled="!form.selectedTime"-->
+            <!--            >-->
+            <!--              <v-icon small>mdi-plus</v-icon>-->
+            <!--              Thêm người-->
+            <!--            </v-btn>-->
           </div>
           <h6 class="my-2">1.2 Lưu lịch sử di chuyển vào server</h6>
           <div class="d-flex gap-2">
-            <v-btn
-              color="primary"
-              small
-              class="mt-4 flex-1"
-              @click="saveMovement"
-            >
+            <v-btn color="primary" small class="flex-1" @click="saveHistory">
               <v-icon small>mdi-database-marker</v-icon>
               Lưu vào server
             </v-btn>
-            <v-btn
-              color="error"
-              small
-              class="flex-1 mt-4"
-              @click="deleteSchedule"
-            >
+            <v-btn color="error" small class="flex-1" @click="deleteSchedule">
               Xoá lịch trình
             </v-btn>
           </div>
-
           <h6 class="my-2">1.3 Phân tích tuyến đường thường xuyên</h6>
           <v-btn block color="primary" small class="mt-4" @click="saveMovement">
             <v-icon x-small class="mr-1">mdi-google-analytics</v-icon>
             Phân tích
           </v-btn>
+        </v-tab-item>
+        <v-tab-item key="cluster" class="pa-4" v-if="isDemo">
           <h6 class="my-2">1.4 Ghép nhóm</h6>
           <v-btn block color="primary" small class="mt-4" @click="">
             <v-icon x-small class="mr-1">mdi-account-multiple</v-icon>
             Ghép nhóm
           </v-btn>
         </v-tab-item>
-        <v-tab-item key="cluster" class="pa-4" v-if="isDemo"></v-tab-item>
         <v-tab-item key="grouping" class="pa-4">
           <h6 class="mb-2">Các nhóm</h6>
           <div>
@@ -265,7 +260,8 @@ import { COLOR_PALETTE, HEADER_CLUSTER_SMALL } from "@/utils/constant";
 import { formattedTime, removeElementAtIndex } from "@/utils/utilities";
 import DatePicker from "@/components/DatePicker";
 import TimePicker from "@/components/TimePicker";
-import { clearMovement } from "@/services/movements";
+import { clearMovement, createMovementMany } from "@/services/movements";
+import moment from "moment";
 
 export default {
   name: "ClusterAdmin",
@@ -277,9 +273,8 @@ export default {
     RoutingMultipleMarkerWithColor2,
   },
   data: () => ({
-    tab: "init",
-    listUser: JSON.parse(localStorage.getItem("locationSet")) || [],
-    cap: 1,
+    tab: "frequent",
+    listUser: JSON.parse(localStorage.getItem("locationSet")) || {},
     HEADER_CLUSTER_SMALL,
     COLOR_PALETTE,
     mapCursor: null,
@@ -288,13 +283,25 @@ export default {
       date: "2022-03-10",
       time: "08:00",
       interval: "5",
+      listTime: JSON.parse(localStorage.getItem("listTime")) || [],
     },
     isDemo: Boolean(JSON.parse(localStorage.getItem("isDemo"))),
   }),
   methods: {
     formattedTime,
     deleteUser(index) {
-      this.listUser = removeElementAtIndex(this.listUser, index);
+      const newListUser = structuredClone(this.listUser);
+      newListUser[this.form.selectedTime] = removeElementAtIndex(
+        newListUser[this.form.selectedTime],
+        index,
+      );
+      this.listUser = newListUser;
+    },
+    deleteHistory(time) {
+      const newListUser = structuredClone(this.listUser);
+      delete newListUser[time];
+      this.listUser = newListUser;
+      this.form.listTime = this.form.listTime.filter((item) => item !== time);
     },
     handleClick(e) {
       this.mapCursor = {
@@ -302,23 +309,35 @@ export default {
         lng: e.latLng.lng(),
       };
     },
+    addCurrentHistory(time) {
+      const newListUser = structuredClone(this.listUser);
+      newListUser[time].locations.push(this.mapCursor);
+      this.listUser = newListUser;
+    },
     addCurrentLocation(index) {
       const newListUser = structuredClone(this.listUser);
-      newListUser[index].location.push(this.mapCursor);
+      newListUser[this.form.selectedTime][index].locations.push(this.mapCursor);
       this.listUser = newListUser;
     },
     updateLocation(value, userIndex) {
       const newListUser = structuredClone(this.listUser);
-      newListUser[userIndex].location[value.index] = value.location;
+      newListUser[this.form.selectedTime][userIndex].locations[value.index] =
+        value.locations;
       this.listUser = newListUser;
     },
     clearLocation(index) {
       const newListUser = structuredClone(this.listUser);
-      newListUser[index].location = [];
+      newListUser[this.form.selectedTime][index].locations = [];
+      this.listUser = newListUser;
+    },
+    clearHistory(time) {
+      const newListUser = structuredClone(this.listUser);
+      newListUser[time].locations = [];
       this.listUser = newListUser;
     },
     async deleteSchedule() {
-      this.listUser = [];
+      this.listUser = {};
+      this.form.listTime = [];
       try {
         this.loading(true);
         await clearMovement();
@@ -329,7 +348,64 @@ export default {
       }
     },
     async saveMovement() {
-      console.log(this.listUser);
+      for (let time of this.listUser) {
+        console.log(time);
+      }
+    },
+    addTime() {
+      if (this.form.time && this.form.date) {
+        const dateTime = `${this.form.date}-${this.form.time}`;
+        if (!this.form.listTime.includes(dateTime)) {
+          this.form.listTime = [...this.form.listTime, dateTime];
+          const newListUser = structuredClone(this.listUser);
+          newListUser[dateTime] = { locations: [] };
+          this.listUser = newListUser;
+        }
+      }
+    },
+    deleteTime() {
+      if (this.form.selectedTime) {
+        const newListUser = structuredClone(this.listUser);
+        delete newListUser[this.form.selectedTime];
+        this.listUser = newListUser;
+        this.form.listTime = this.form.listTime.filter(
+          (item) => item !== this.form.selectedTime,
+        );
+        this.form.selectedTime = null;
+      }
+    },
+    initSetLocation() {
+      if (this.form.selectedTime) {
+        const newListUser = structuredClone(this.listUser);
+        newListUser[this.form.selectedTime] = [{ locations: [] }];
+        this.listUser = newListUser;
+      }
+    },
+    async saveHistory() {
+      try {
+        this.loading(true);
+        for (let time in this.listUser) {
+          await createMovementMany({
+            t_start: moment(
+              `${this.form.date}-${this.form.time}`,
+              "YYYY-MM-DD-HH:mm",
+            )
+              .utc()
+              .format(),
+            t_interval: 5,
+            list: [
+              {
+                user_id: "testId",
+                locations: this.listUser[time].locations,
+              },
+            ],
+          });
+        }
+        this.loading(false);
+        this.alert.success("Lưu thành công");
+      } catch (e) {
+        this.alert.error("Lưu thất bại");
+      }
     },
   },
   mounted() {},
@@ -342,6 +418,11 @@ export default {
         localStorage.setItem("locationSet", JSON.stringify(value));
       },
       deep: true,
+    },
+    ["form.listTime"]: {
+      handler: (value) => {
+        localStorage.setItem("listTime", JSON.stringify(value));
+      },
     },
   },
 };
